9121f647629aa74cd9fa648e01b41c4aa953c193
Lindsey Adams <linzma87@gmail.com>
2016-10-04 17:17:36 -0400

    Getting info!

--- js/main.js
+++ js/main.js
@@ -1,18 +1,67 @@
-// org container
+$(document).ready(function() {
 
 
-// org name
 
-var username = '';
+    // get username from input
 
-// org avatar
+    $('form').submit(function(event) {
+        event.preventDefault();
+        var username = $('#username').val();
+        requestInfo(username);
+    });
 
-function getDetails(username) {
+    // handle errors
 
-  this.info = {
-    avatar: username.avatar_url,
-    orgName: username.login,
-  };
+    function handleError(errorObject, textStatus, error) {
+      console.log(errorObject, textStatus, error);
+    }
+
+    // get avatar and org name
+
+    function orgDetails(orgObject) {
+
+      var newOrg = orgObject[0];
+
+        console.log(newOrg);
+
+        this.info = {
+            avatar: newOrg.avatar_url,
+            orgName: newOrg.login,
+        };
+
+        console.log(this.info.orgName);
+
+    }
+
+    // request info from github
+
+    function requestInfo(username) {
+
+      var token = "9936450c6f508a15de5b6741d6bc7cee233a9974";
+
+      $.ajax({
+            "async": true,
+            "crossDomain": true,
+            "type": "GET",
+            "url": "https://api.github.com/users/" + username + "/orgs?access_token=" + token,
+            "success": orgDetails,
+            "error": handleError
+
+    });
+  }
 
 
-}
+    // org container
+
+
+    // org name
+
+
+
+
+
+    // org avatar
+
+
+
+});
