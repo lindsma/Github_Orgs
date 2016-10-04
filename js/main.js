dc967c0a176fdd6d226cd0d51ad689992809cfd2
Lindsey Adams <linzma87@gmail.com>
2016-10-04 15:32:36 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// org container
+
+
+// org name
+
+var username = '';
+
+// org avatar
+
+function getDetails(username) {
+
+  this.info = {
+    avatar: username.avatar_url,
+    orgName: username.login,
+  };
+
+
+}
